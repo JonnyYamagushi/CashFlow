03fc71b8d2bcc2f35fd713ac5226ed60a1bbc386
João Guilherme Ferreira Passos <joao.passos5@etec.sp.gov.br>
2025-06-25 19:12:37 -0300

    Validate
    Implementação de Validate Expenses

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,9 +8,31 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
+
         return new ResponseRegisterExpenseJson
         {
             Title = request.Title
         };
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        if (string.IsNullOrEmpty(request.Title.Trim()))
+        {
+            throw new ArgumentException("Title cannot be empty.");
+        }
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+        if (DateTime.Compare(request.Date, DateTime.UtcNow) > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+        if (Enum.IsDefined(typeof(PaymentType), request.PaymentType) == false)
+        {
+            throw new ArgumentException("Payment type is not valid.");
+        }
     }
 }
--- src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -8,5 +8,5 @@ public class RequestRegisterExpenseJson
     public string Description { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public decimal Amount { get; set; }
-    public PaymentType MyProperty { get; set; }
+    public PaymentType PaymentType { get; set; }
 }
