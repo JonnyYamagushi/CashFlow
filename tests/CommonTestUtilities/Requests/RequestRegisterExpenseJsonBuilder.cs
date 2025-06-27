90dfed4363c1acdb080c71811532e85f11eeb154
João Guilherme Ferreira Passos <joao.passos5@etec.sp.gov.br>
2025-06-27 20:23:23 -0300

    Adicionar projetos de teste e utilitários para validação de despesas
    Os projetos Validators.Tests e CommonTestUtilities foram adicionados à solução. Utilitários de teste foram implementados para a criação de solicitações de despesas e testes unitários foram criados para o RegisterExpenseValidator para abranger cenários de validação.

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {        
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Date, f => f.Date.Past())
+            .RuleFor(r => r.Amount, f => f.Finance.Amount(decimals: 2))
+            .RuleFor(r => r.Description, f => f.Lorem.Sentence())
+            .RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
