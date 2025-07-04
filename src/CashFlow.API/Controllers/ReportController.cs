fb0437b0a925811d4cdd82c70df7aa259df97e6b
João Guilherme Ferreira Passos <joao.passos5@etec.sp.gov.br>
2025-07-03 21:13:38 -0300

    Adicionar recurso de geração de relatórios de despesas em PDF
    Introduzida a geração de relatórios em PDF para despesas, incluindo novo use case, interface, recursos de fonte e registro de injeção de dependência. O ReportController foi atualizado com um novo endpoint para baixar relatórios em PDF. Adicionadas strings de recursos localizadas para títulos e totais de relatórios e fontes personalizadas incorporadas para renderização em PDF.

--- src/CashFlow.API/Controllers/ReportController.cs
+++ src/CashFlow.API/Controllers/ReportController.cs
@@ -1,6 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
-using CashFlow.Communication.Requests;
-using Microsoft.AspNetCore.Http;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -22,5 +21,20 @@ public class ReportController : ControllerBase
         }
 
         return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+    }
+
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf([FromServices] IGenerateExpensesReportPdfUseCase useCase, [FromQuery] DateOnly month)
+    {
+        byte[] file = await useCase.Execute(month);
+
+        if (file.Length == 0)
+        {
+            return NoContent();
+        }
+
+        return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,86 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
+using DocumentFormat.OpenXml.Office.CustomUI;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Fonts;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+
+internal class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private const string CURRENCY_SYMBOL = "R$";
+    private readonly IExpenseReadOnlyRepository _repository;
+
+    public GenerateExpensesReportPdfUseCase(IExpenseReadOnlyRepository repository)
+    {
+        _repository = repository;
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+    }
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _repository.GetByMonth(month);
+        if (expenses.Count == 0)
+        {
+            return [];
+        }
+
+        var document = CreateDocument(month);
+        var  page = CreatePage(document);
+
+        var Paragraph = page.AddParagraph();
+        
+        var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+        Paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALEWAY_REGULAR, Size = 15 });
+        Paragraph.AddLineBreak();
+
+        var total = expenses.Sum(x => x.Amount);
+        Paragraph.AddFormattedText($"{CURRENCY_SYMBOL} {total:N2}", new Font { Name = FontHelper.RALEWAY_BLACK, Size = 50 });
+
+        return RenderDocument(document);
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+        document.Info.Author = "CashFlow";
+
+        var style = document.Styles["Normal"];
+        style.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+
+        return document;
+    }
+
+    private Section CreatePage(Document document)
+    {
+        var section = document.AddSection();
+        section.PageSetup = document.DefaultPageSetup.Clone();
+        
+        section.PageSetup.PageFormat = PageFormat.A4;
+
+        section.PageSetup.LeftMargin = 40;
+        section.PageSetup.RightMargin = 40;
+        section.PageSetup.TopMargin = 80;
+        section.PageSetup.BottomMargin = 80;
+
+        return section;
+    }
+
+    private byte[] RenderDocument(Document document)
+    {
+        var renderer = new PdfDocumentRenderer
+        {
+            Document = document
+        };
+
+        renderer.RenderDocument();
+
+        using var file = new MemoryStream();
+        renderer.PdfDocument.Save(file);
+        return file.ToArray();
+    }
+}
