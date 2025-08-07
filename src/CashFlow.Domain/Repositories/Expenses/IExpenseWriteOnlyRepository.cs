d6b21f00a0fcdecfa1234d4d78e427f9afc8036e
João Guilherme Ferreira Passos <joao.passos5@etec.sp.gov.br>
2025-08-07 19:33:02 -0300

    Iniciando Migrations

--- src/CashFlow.Domain/Repositories/Expenses/IExpenseWriteOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpenseWriteOnlyRepository.cs
@@ -5,10 +5,5 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpenseWriteOnlyRepository
 {
     Task Add(Expense expense);
-    /// <summary>
-    /// This function returns true if the expense was deleted successfully, otherwise false.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
     Task<bool> Delete(long id);
 }
